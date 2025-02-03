21ff9a948819524e509a94a3a839815b1ff78120
Sayjan J Singh <118334094+Sia714@users.noreply.github.com>
2025-02-03 12:47:54 +0530

    Time: 7 ms (36.25%) | Memory: 26.6 MB (47.10%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -8,26 +8,7 @@ public:
     }
     
     bool validPalindrome(string s) {
-        // int i=0,j=s.length();
-        // int c=0;
-        // while(i<j){
-        //     if (s[i] !=s[j]){
-        //         cout<<i<<':'<<j<<endl;
-        //         if(s[i+1]==s[j] && c<1){
-        //             i++;
-        //             c++;
-        //         }
-        //         else if(s[j-1]==s[i] && c<1){
-        //             j--;
-        //             c++;
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }
-        //     i++; j--; 
-        // }
-        // return true;
+
 
         int i=0,j=s.size()-1;
         while(i<j && s[i]==s[j]){
