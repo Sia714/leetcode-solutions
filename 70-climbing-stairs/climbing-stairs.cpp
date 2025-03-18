1b9644e0ad011ac175e3477236610292f90a2daa
Sayjan J Singh <118334094+Sia714@users.noreply.github.com>
2025-03-18 21:07:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -14,8 +14,12 @@ public:
         return dp[n];
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(dp,n);
+        vector<int> dp(n+2,0);
+        dp[0]=0;dp[1]=1;
+        for(int i=2;i<=n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n+1];
 
     }
 };
