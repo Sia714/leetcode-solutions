a9ea2802f3664d95dc7c15fbe55a56c041cbacfc
Sayjan J Singh <118334094+Sia714@users.noreply.github.com>
2025-03-18 21:22:22 +0530

    Time: 3 ms (19.15%) | Memory: 8.8 MB (56.09%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,10 +3,9 @@ public:
     int numDecodings(string s) {
         if (s[0]=='0'){return 0;}
         int n=s.length();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
         dp[0]=dp[1]=1;
         for(int i=1; i<n;i++){
-            dp[i+1]=0;
             if (s[i]!='0'){
                 dp[i+1]+=dp[i];
             }
