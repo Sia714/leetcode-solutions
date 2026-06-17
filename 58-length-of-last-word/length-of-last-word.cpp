7129fe1e1a4cc84966c514bac0e5ab5e9bdecc22
Sayjan J Singh <118334094+Sia714@users.noreply.github.com>
2026-06-17 19:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (33.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,17 +3,6 @@ public:
     int lengthOfLastWord(string s) {
         int res=0;
         int i=s.length()-1;
-        if(s.length() < 0) {
-            return 0;
-        }
-        if(s.length() == 1){
-            if(s[0] == ' ') {
-                return 0;
-            }
-            else {
-                return 1;
-            }
-        }
         while(i >= 0 && s[i]==' '){
             i--;
         }
